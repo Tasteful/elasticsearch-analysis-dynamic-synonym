03fe45d179935a705109a0ae24a798e1e89d1750
Patric Forsgard <patric@tasteful.se>
2023-04-12 22:14:10 +0200

    Update symbol namespace

--- src/main/java/com/bellszhu/elasticsearch/plugin/DynamicSynonymPlugin.java
+++ src/main/java/com/bellszhu/elasticsearch/plugin/DynamicSynonymPlugin.java
@@ -9,7 +9,6 @@ import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.component.LifecycleComponent;
 import org.elasticsearch.common.io.stream.NamedWriteableRegistry;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.NamedXContentRegistry;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.env.NodeEnvironment;
 import org.elasticsearch.index.IndexSettings;
